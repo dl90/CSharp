ebbf63e608d2f9767bbade428cadb27482b45833
dl90 <donli90@hotmail.com>
2020-09-11 23:08:13 -0700

    calc

---
+++ basics.md
@@ -0,0 +1,88 @@
+# C# basics
+
+namespace used to organize your code. It is a container for classes and other namespaces
+
+```c#
+int num = 5;              // 32 bit signed
+long numL = 2147483647L;  // 64 bit signed
+float numF = 5.75F;       // 3.4e38 (approx)
+double numD = 5.99D;      // 3.4e308 (approx)
+decimal numDec = 1.234;   // 28~29 sig-figs (precision)
+
+char myLetter = 'D';      // ASCII chars + [/n... etc]
+string myText = "Hello";
+
+bool myBool = true;
+
+const int myNum = 15;
+```
+
+```c#
+// implicit casting [char -> int -> long -> float -> double]
+int myInt = 9;
+double myDouble = myInt;    // int -> double
+
+// explicit casting [double -> float -> long -> int -> char]
+double myDouble = 9.78;
+int myInt = (int) myDouble; // double -> int
+
+/*
+  Convert.ToBoolean
+  Convert.ToDouble
+  Convert.ToString
+  Convert.ToInt32 (int)
+  Convert.ToInt64 (long)
+*/
+```
+
+---
+
+```c#
+using System;
+
+namespace test
+{
+  class Program
+  {
+
+    // void == method has no return
+    static void Method()
+    {
+      for(int i = 0; i < 10; i++) {
+        Console.WriteLine(string i)
+      }
+    }
+
+    static void MethodArg(int count, string arg = "Default")
+    {
+      Console.WriteLine($"{count}\n{arg}")
+    }
+
+    // int == return type int
+    static int MyMethod(int x)s
+    {
+      return 5 + x;
+    }
+
+    // method overloading
+    static int PlusMethod(int x, int y)
+    {
+      return x + y;
+    }
+
+    static double PlusMethod(double x, double y)
+    {
+      return x + y;
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      string userName = Console.ReadLine();
+      Console.WriteLine($"hi {username}");
+      Method();
+      MethodArg(1, "Test");
+    }
+  }
+}
+```
---
+++ calc/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num_1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.num_2 = new System.Windows.Forms.Button();
+            this.num_3s = new System.Windows.Forms.Button();
+            this.num_4 = new System.Windows.Forms.Button();
+            this.num_5 = new System.Windows.Forms.Button();
+            this.num_6 = new System.Windows.Forms.Button();
+            this.num_7 = new System.Windows.Forms.Button();
+            this.num_8 = new System.Windows.Forms.Button();
+            this.num_9 = new System.Windows.Forms.Button();
+            this.num_0 = new System.Windows.Forms.Button();
+            this.enter = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num_1
+            // 
+            this.num_1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_1.Location = new System.Drawing.Point(300, 420);
+            this.num_1.Name = "num_1";
+            this.num_1.Size = new System.Drawing.Size(100, 80);
+            this.num_1.TabIndex = 0;
+            this.num_1.Text = "1";
+            this.num_1.UseVisualStyleBackColor = true;
+            this.num_1.Click += new System.EventHandler(this.num_1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(14, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1004, 296);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // num_2
+            // 
+            this.num_2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.num_2.Location = new System.Drawing.Point(420, 420);
+            this.num_2.Name = "num_2";
+            this.num_2.Size = new System.Drawing.Size(100, 80);
+            this.num_2.TabIndex = 2;
+            this.num_2.Text = "2";
+            this.num_2.UseVisualStyleBackColor = true;
+            this.num_2.Click += new System.EventHandler(this.num_2_Click);
+            // 
+            // num_3s
+            // 
+            this.num_3s.Location = new System.Drawing.Point(540, 420);
+            this.num_3s.Name = "num_3s";
+            this.num_3s.Size = new System.Drawing.Size(100, 80);
+            this.num_3s.TabIndex = 3;
+            this.num_3s.Text = "3";
+            this.num_3s.UseVisualStyleBackColor = true;
+            this.num_3s.Click += new System.EventHandler(this.num_3s_Click);
+            // 
+            // num_4
+            // 
+            this.num_4.Location = new System.Drawing.Point(300, 520);
+            this.num_4.Name = "num_4";
+            this.num_4.Size = new System.Drawing.Size(100, 80);
+            this.num_4.TabIndex = 4;
+            this.num_4.Text = "4";
+            this.num_4.UseVisualStyleBackColor = true;
+            this.num_4.Click += new System.EventHandler(this.num_4_Click);
+            // 
+            // num_5
+            // 
+            this.num_5.Location = new System.Drawing.Point(420, 520);
+            this.num_5.Name = "num_5";
+            this.num_5.Size = new System.Drawing.Size(100, 80);
+            this.num_5.TabIndex = 5;
+            this.num_5.Text = "5";
+            this.num_5.UseVisualStyleBackColor = true;
+            this.num_5.Click += new System.EventHandler(this.num_5_Click);
+            // 
+            // num_6
+            // 
+            this.num_6.Location = new System.Drawing.Point(540, 520);
+            this.num_6.Name = "num_6";
+            this.num_6.Size = new System.Drawing.Size(100, 80);
+            this.num_6.TabIndex = 6;
+            this.num_6.Text = "6";
+            this.num_6.UseVisualStyleBackColor = true;
+            this.num_6.Click += new System.EventHandler(this.num_6_Click);
+            // 
+            // num_7
+            // 
+            this.num_7.Location = new System.Drawing.Point(300, 620);
+            this.num_7.Name = "num_7";
+            this.num_7.Size = new System.Drawing.Size(100, 80);
+            this.num_7.TabIndex = 7;
+            this.num_7.Text = "7";
+            this.num_7.UseVisualStyleBackColor = true;
+            this.num_7.Click += new System.EventHandler(this.num_7_Click);
+            // 
+            // num_8
+            // 
+            this.num_8.Location = new System.Drawing.Point(420, 620);
+            this.num_8.Name = "num_8";
+            this.num_8.Size = new System.Drawing.Size(100, 80);
+            this.num_8.TabIndex = 8;
+            this.num_8.Text = "8";
+            this.num_8.UseVisualStyleBackColor = true;
+            this.num_8.Click += new System.EventHandler(this.num_8_Click);
+            // 
+            // num_9
+            // 
+            this.num_9.Location = new System.Drawing.Point(540, 620);
+            this.num_9.Name = "num_9";
+            this.num_9.Size = new System.Drawing.Size(100, 80);
+            this.num_9.TabIndex = 9;
+            this.num_9.Text = "9";
+            this.num_9.UseVisualStyleBackColor = true;
+            this.num_9.Click += new System.EventHandler(this.num_9_Click);
+            // 
+            // num_0
+            // 
+            this.num_0.Location = new System.Drawing.Point(420, 720);
+            this.num_0.Name = "num_0";
+            this.num_0.Size = new System.Drawing.Size(100, 80);
+            this.num_0.TabIndex = 10;
+            this.num_0.Text = "0";
+            this.num_0.UseVisualStyleBackColor = true;
+            this.num_0.Click += new System.EventHandler(this.num_0_Click);
+            // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(540, 720);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(100, 80);
+            this.enter.TabIndex = 11;
+            this.enter.Text = "enter";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(300, 720);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(100, 80);
+            this.delete.TabIndex = 12;
+            this.delete.Text = "delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(759, 420);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(100, 80);
+            this.add.TabIndex = 13;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Location = new System.Drawing.Point(759, 520);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(100, 80);
+            this.subtract.TabIndex = 14;
+            this.subtract.Text = "-";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(759, 620);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(100, 80);
+            this.multiply.TabIndex = 15;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(759, 720);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(100, 80);
+            this.divide.TabIndex = 16;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(1032, 883);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.num_0);
+            this.Controls.Add(this.num_9);
+            this.Controls.Add(this.num_8);
+            this.Controls.Add(this.num_7);
+            this.Controls.Add(this.num_6);
+            this.Controls.Add(this.num_5);
+            this.Controls.Add(this.num_4);
+            this.Controls.Add(this.num_3s);
+            this.Controls.Add(this.num_2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.num_1);
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num_1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button num_2;
+        private System.Windows.Forms.Button num_3s;
+        private System.Windows.Forms.Button num_4;
+        private System.Windows.Forms.Button num_5;
+        private System.Windows.Forms.Button num_6;
+        private System.Windows.Forms.Button num_7;
+        private System.Windows.Forms.Button num_8;
+        private System.Windows.Forms.Button num_9;
+        private System.Windows.Forms.Button num_0;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+    }
+}
+
